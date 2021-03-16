e2881b2702521e55e71a80bd6280a5b56df05fd7
Bilal Belagam <bilal.belagam@gmail.com>
2021-03-15 21:15:05 -0400

    first night of getting back into it, took me twenty minutes to realize a function was missing a return statement

---
+++ javascript/day001.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+function multiply(a, b) {
+  return a * b
+}
+
+let solution = multiply(2, 4)
+console.log(solution)
+
+// function was missing a return statement originally
